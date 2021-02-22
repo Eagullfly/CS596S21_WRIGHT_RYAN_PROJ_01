ec0ad940f5b88f05c6dad32c8c7ae20dd31479df
Ryan Wright <rwright3702@sdsu.edu>
2021-02-21 19:10:46 -0800

    Began writing scripts.

--- Assets/Editor/PGCTerrainEditor.cs
+++ Assets/Editor/PGCTerrainEditor.cs
@@ -1,9 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class PGCTerrainEditor : MonoBehaviour
+[CustomEditor(typeof(PGCTerrain))]
+[CanEditMultipleObjects]
+public class PGCTerrainEditor : Editor
 {
+    SerializedProperty heightMapImage;
+    SerializedProperty heightScale;
+    SerializedProperty resetTerrain;
+    SerializedProperty perlinMountainHi;
+    SerializedProperty perlinMountainLo;
+    SerializedProperty perlinTileSize;
+
+    bool showPGCValues = false;
+    bool showPerlinValues = false;
+
+    private void OnEnable()
+    {
+        heightMapImage = serializedObject.FindProperty("heightMapImage");
+        heightScale = serializedObject.FindProperty("heightScale");
+        resetTerrain = serializedObject.FindProperty("resetTerrain");
+        perlinMountainHi = serializedObject.FindProperty("perlinMountainHi");
+        perlinMountainLo = serializedObject.FindProperty("perlinMountainLo");
+        perlinTileSize = serializedObject.FindProperty("perlinTileSize");
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        PGCTerrainEditor terrain = (PGCTerrain)target;
+
+        EditorGUILayout.LabelField("PGC Terrain Editor", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(resetTerrain);
+
+        showPGCValues = EditorGUILayout.Foldout(showPGCValues, "PGC Values");
+        if (showPGCValues)
+        {
+            EditorGUILayout.LabelField("", OnInspectorGUI.skin.horizontalSlider);
+            GUILayout.Label("Set PGC Values here ", EditorStyle.boldLabel);
+            EditorGUILayout.PropertyField(heightScale);
+            if(GUILayout.Button("Reset Terrain"))
+            {
+                terrain.resetTerrain();
+            }
+        }
+
+        showPerlinValues = EditorGUILayout.Foldout(showPerlinValues, "Perlin Values");
+        if (showPerlinValues)
+        {
+            GUILayout.Label("Set Perlin ", EditorStyles.boldLabel);
+            EditorGUILayout.Slider(perlinMountainHi, 10, 600, new GUIContent("Top of Perlin Mountains"));
+            EditorGUILayout.Slider(perlinMountainLo, -50, 9.8f, new GUIContent("Bottom of Perlin Mountains"));
+            EditorGUILayout.IntSlider(perlinTileSize, 1, 129, new GUIContent("Terrain Tile Size"));
+            if (GUILayout.Button("Add Perlin"))
+            {
+                terrain.PerlinTerrain();
+            }
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PGCTerrain.cs
+++ Assets/Scripts/PGCTerrain.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
+[ExecuteInEditMode]
 public class PGCTerrain : MonoBehaviour
 {
+
+    public Terrain terrain;
+    public TerrainData terrainData;
+    private float[,] heightMap;
+    int xSize, zSize;
+
+    public float heightScale = 0f;
+    public Texture2D heightMapImage;
+    public bool resetTerrain = true;
+
+    public float perlinMountainHi;
+    public float perlinMountainLo;
+    public int perlinTileSize;
+
+    [System.Obsolete]
+    void OnEnable()
+    {
+        terrain = GetComponent<terrain>();
+        terrainData = terrain.terrainData;
+        xSize = (int)terrain.terrainData.size.x;
+        zSize = (int)terrainData.size.z;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SetHeightScale()
+    {
+        Debug.Log("Height Scale = " + heightScale);
     }
 }
